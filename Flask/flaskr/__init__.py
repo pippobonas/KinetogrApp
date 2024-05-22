5ac74ae3427342e225d4c4a3fabaf33abe934c71
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-22 02:21:27 +0200

    fix:package import nomenclature

--- Flask/flaskr/__init__.py
+++ Flask/flaskr/__init__.py
@@ -1,5 +1,5 @@
-from app import create_app
-from config import Config
-from db import db, migrate
+from .app import create_app
+from .config import Config
+from .db import db, migrate
 
 app=create_app(Config,db,migrate)
