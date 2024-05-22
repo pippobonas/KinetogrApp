a4b2aad176491e56d3c4b45a6c88b69fbdb5ffa7
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-23 01:10:41 +0200

    fix: factory app work for most cases

--- Flask/flaskr/app.py
+++ Flask/flaskr/app.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 def create_app(config,db,migrate):
     app = Flask(__name__)
-    app.config.from_object(config)
-    db.init_app(app)
-    migrate.init_app(app, db)
+    if(config != None):
+        app.config.from_object(config)
+    else:
+        app.config['TESTING'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kine.db'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['WTF_CSRF_ENABLED'] = False
+    if db != None:
+        db.init_app(app)
+        if migrate != None:
+            migrate.init_app(app, db)
     
     
     return app
