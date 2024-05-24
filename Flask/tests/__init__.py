6f1cf0938f4cc7fdc3158051eefc6a385b49e65c
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-24 21:52:11 +0200

    doc: FLask/tests/__init__.py

--- Flask/tests/__init__.py
+++ Flask/tests/__init__.py
@@ -1,24 +1,33 @@
+'''
+- set path /../flaskr to sys.path for import modules from flaskr
+- setting fixtures for testing
+'''
 import sys
 import os
+import pytest
+from flask import Flask
+from .configtest import TestConfig as conf
 
-"""Add the parent directory to the sys.path so that the tests can import the modules from the src directory"""
+#setting path
 current_directory = os.path.dirname(os.path.realpath(__file__))
 flaskr_directory = os.path.dirname(current_directory+'/../flaskr')
-
 sys.path.append(current_directory)
 sys.path.append(flaskr_directory)
 
-import pytest
-from configtest import TestConfig as conf
-from flask import Flask
 #fixtures
-
 @pytest.fixture(scope='session')
 def app ():
+    '''
+    fixture for flask app
+    -dont have anthing
+    '''
     app = Flask(__name__)
     app.config.from_object(conf)
     return app
 
 @pytest.fixture(scope='session')
 def client(app):
+    '''
+    fixture for flask client
+    '''
     return app.test_client()
