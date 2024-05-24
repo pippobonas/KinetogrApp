036844d4c2c8962dd8d553edc485ee6981ce63a0
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-24 20:09:43 +0200

    docs: flaskr/movie_model

--- Flask/flaskr/movie_model.py
+++ Flask/flaskr/movie_model.py
@@ -1,6 +1,20 @@
+'''
+this class is used to define a model for the movie table in the database.
+'''
 from flaskr.db import db
 
 class Movie(db.Model):
+    '''
+    this class have the attributes of the movie
+    table_name: movies
+    id: primary key
+    title: movie title
+    director: movie director
+    plot: movie plot
+    release_year: movie release year
+    poster_path: movie poster path 
+    services_available: movie services available 
+    '''
     __tablename__ = 'movies'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), nullable=False)
@@ -9,4 +23,3 @@ class Movie(db.Model):
     release_year = db.Column(db.Integer, nullable=False)
     poster_path = db.Column(db.String(4096))
     services_available = db.Column(db.String(64), nullable=False)
-
