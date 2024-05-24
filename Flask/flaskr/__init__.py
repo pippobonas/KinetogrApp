6e95fdf1c17deeeff8ca1338a904fe4dacfd378b
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-24 18:23:39 +0200

    doc: add docs

--- Flask/flaskr/__init__.py
+++ Flask/flaskr/__init__.py
@@ -3,5 +3,7 @@ from .config import Config
 from .db import db, migrate
 from .rest_api import movie_routes
 
+'''initialize the server with the configuration and the database,migration
+    and start the routes movie'''
 app=create_app(Config,db,migrate)
 movie_routes(app)
