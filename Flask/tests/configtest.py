98d30f496ed7404cf03bf582ad5054db3887a495
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-24 21:57:34 +0200

    doc: Flask/tests/configtest

--- Flask/tests/configtest.py
+++ Flask/tests/configtest.py
@@ -1,4 +1,10 @@
+'''
+module for configuration of flask
+'''
 class TestConfig:
+    '''
+    default clas configuration for testing
+    '''
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
