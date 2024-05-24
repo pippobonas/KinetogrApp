64824c6af958e162aacfe4049b0a863663980529
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-24 19:56:19 +0200

    docs: flaskr/config.py

--- Flask/flaskr/config.py
+++ Flask/flaskr/config.py
@@ -1,4 +1,11 @@
+'''
+This file contains the configuration for the Flask app.
+'''
 class Config:
+    '''
+    It sets the database URI to a local SQLite database and
+    disables the modification tracking feature of SQLAlchemy.
+    '''
     TESTING = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///KinetogrApp.db?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
