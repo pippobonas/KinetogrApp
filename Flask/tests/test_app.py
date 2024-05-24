e8db65e995b947451cac0a028d3f39cda516c7df
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-24 22:08:26 +0200

    doc: Flask/tests/tests_app.py

--- Flask/tests/test_app.py
+++ Flask/tests/test_app.py
@@ -1,10 +1,17 @@
+'''
+This file contains tests for create_app function in app.py
+'''
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import pytest
 from flaskr.app import create_app
 from configtest import TestConfig as conf
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
 
 class Test_create_app:
+    '''
+    class for testing create_app function
+    '''
     setup = [{'db': SQLAlchemy(), 'migrate': Migrate(), 'conf': conf},
              {'db': SQLAlchemy(), 'migrate': Migrate(), 'conf': None},
              {'db': SQLAlchemy(), 'migrate': None, 'conf': conf},
@@ -13,14 +20,14 @@ class Test_create_app:
              {'db': None, 'migrate': Migrate(), 'conf': None},
              {'db': None, 'migrate': None, 'conf': conf},
              {'db': None, 'migrate': None, 'conf': None}]
-    
-    
     @pytest.mark.parametrize('setup', setup )
     def test_app(self, setup):
+        '''
+        parametrized test for create_app function
+        '''
         app = create_app(conf, setup['db'],setup['migrate'])
         assert app is not None
         assert app.config['TESTING'] == conf.TESTING
         assert app.config['SQLALCHEMY_DATABASE_URI'] == conf.SQLALCHEMY_DATABASE_URI
         assert app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] == conf.SQLALCHEMY_TRACK_MODIFICATIONS
         assert app.config['WTF_CSRF_ENABLED'] == conf.WTF_CSRF_ENABLED
-        
