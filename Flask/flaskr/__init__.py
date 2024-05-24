3c47d27f738b8ed188f3aa5ae1dd10bfd65c1bdd
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-24 18:28:03 +0200

    docs: flaskr/__init__.py

--- Flask/flaskr/__init__.py
+++ Flask/flaskr/__init__.py
@@ -1,9 +1,11 @@
+'''initialize and start the server and set routes on'''
 from .app import create_app
 from .config import Config
 from .db import db, migrate
 from .rest_api import movie_routes
 
-'''initialize the server with the configuration and the database,migration
-    and start the routes movie'''
+#initialize the server with the configuration,db,migration '''
 app=create_app(Config,db,migrate)
+
+#initialize the routes for the server
 movie_routes(app)
