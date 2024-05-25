e30a8e7c9cd2f22a7c0b040ae7b1a2cbb696263e
PippoBonas <bonaventuragiuseppe1997@gmail.com>
2024-05-25 08:38:57 +0200

    feat: create methods in class flaskr/config.py __repr__ and __str__

--- Flask/flaskr/config.py
+++ Flask/flaskr/config.py
@@ -9,3 +9,19 @@ class Config:
     TESTING = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///KinetogrApp.db?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    def __repr__(self):
+        '''rappresentation of the object used for debugging '''
+        temp_repr = ['Config(' +
+        'TESTING={self.TESTING}' +
+        'SQLALCHEMY_DATABASE_URI={self.SQLALCHEMY_DATABASE_URI}' +
+        'SQLALCHEMY_TRACK_MODIFICATIONS={self.SQLALCHEMY_TRACK_MODIFICATIONS}' +
+        ')']
+        return temp_repr[0]
+    def __str__(self):
+        '''rappresentation of the object used for printing '''
+        temp_str = ['Config(\n' +
+        'TESTING={self.TESTING}\n' +
+        'SQLALCHEMY_DATABASE_URI={self.SQLALCHEMY_DATABASE_URI}\n' +
+        'SQLALCHEMY_TRACK_MODIFICATIONS={self.SQLALCHEMY_TRACK_MODIFICATIONS}\n' +
+        ')']
+        return temp_str[0]
